067543b5497f17b94627043990975be9cce6d0a3
Jagadeesh-Siva <142863935+Jagadeesh-Siva@users.noreply.github.com>
2024-03-23 21:38:00 +0530

    Update MUX4x1.v

--- MUX4x1 using MUX2x1 - Day1/MUX4x1.v
+++ MUX4x1 using MUX2x1 - Day1/MUX4x1.v
@@ -1,4 +1,10 @@
 `timescale 1ns / 1ps
+
+module MUX2x1(I,S,F);
+input [1:0]I; input S; output F;
+assign F = S?I[1]:I[0];
+endmodule
+
 module MUX4x1(I,S,F);
 input [3:0]I; input [1:0]S; output F; wire w1,w2;
 MUX2x1 X0({I[0],I[1]},S[0],w1);
