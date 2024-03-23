b4a6ff916eabba4375851806b46a3e1112921a77
Jagadeesh-Siva <142863935+Jagadeesh-Siva@users.noreply.github.com>
2024-03-23 09:50:13 +0530

    Update MUX16x1.v

--- MUX4x1 using MUX2x1 - Day1/MUX16x1.v
+++ MUX4x1 using MUX2x1 - Day1/MUX16x1.v
@@ -1,1 +1,24 @@
-
+`timescale 1ns / 1ps
+module MUX16x1(I,S,F);
+input [15:0]I; input [3:0]S; output reg F;
+always@(*) begin
+case(S) 
+4'b0000:F=I[0];
+4'b0001:F=I[1];
+4'b0010:F=I[2];
+4'b0011:F=I[3];
+4'b0100:F=I[4];
+4'b0101:F=I[5];
+4'b0110:F=I[6];
+4'b0111:F=I[7];
+4'b1000:F=I[8];
+4'b1001:F=I[9];
+4'b1010:F=I[10];
+4'b1011:F=I[11];
+4'b1100:F=I[12];
+4'b1101:F=I[13];
+4'b1110:F=I[14];
+4'b1111:F=I[15];
+endcase
+end
+endmodule
