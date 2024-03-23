fd4d685e9c9c5924156921fdd0d0ea49d9aec994
Jagadeesh-Siva <142863935+Jagadeesh-Siva@users.noreply.github.com>
2024-03-23 09:46:28 +0530

    Update MUX4x1_TB.v

--- MUX4x1 using MUX2x1 - Day1/MUX4x1_TB.v
+++ MUX4x1 using MUX2x1 - Day1/MUX4x1_TB.v
@@ -1,1 +1,10 @@
-
+module MUX4x1_TB;
+reg [3:0]I; reg [1:0]S; wire F;
+MUX4x1 X1(I,S,F);
+initial begin
+{I,S}=6'b100000;#5
+{I,S}=6'b010001;#5
+{I,S}=6'b001010;#5
+{I,S}=6'b000111;#5
+$stop; end
+endmodule
