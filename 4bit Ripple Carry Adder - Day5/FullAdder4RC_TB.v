870cde7db23ef74a8f3501eaab0a260dcebc337c
Jagadeesh-Siva <142863935+Jagadeesh-Siva@users.noreply.github.com>
2024-03-25 18:05:07 +0530

    Update FullAdder4RC_TB.v

--- 4bit Ripple Carry Adder - Day5/FullAdder4RC_TB.v
+++ 4bit Ripple Carry Adder - Day5/FullAdder4RC_TB.v
@@ -1,1 +1,9 @@
-
+module FullAdder4RC_TB;
+reg [3:0]A,B; reg Cin; wire [3:0]Sum; wire Cout;
+FullAdder4RC Y1(A,B,Cin,Sum,Cout);
+initial begin
+{A,B} = 8'h34; Cin=0; #5
+{A,B} = 8'hff; Cin=1; #5
+{A,B} = 8'h7a; Cin=0; #5
+{A,B} = 8'hff; Cin=0; #5
+$stop; end endmodule
