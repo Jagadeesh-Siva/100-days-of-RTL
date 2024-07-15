5d18c589eabdbb149a26a48995e99cff593c6ff6
Jagadeesh-Siva <142863935+Jagadeesh-Siva@users.noreply.github.com>
2024-07-15 10:32:44 +0530

    Update Sub1sCompliment.v

--- [Day-11] : Binary Compliment Subtractors/Sub1sCompliment.v
+++ [Day-11] : Binary Compliment Subtractors/Sub1sCompliment.v
@@ -1,1 +1,28 @@
+module Sub1sCompliment(A,B,Difference,Signbit);
+input [3:0]A;
+input [3:0]B;
+output reg [3:0]Difference;
+output reg Signbit;
+reg carry;
+reg [3:0]OnesCompliment;
+reg [3:0]sum;
 
+always@*
+begin
+ OnesCompliment = ~B;
+ {carry,sum} = A + OnesCompliment;
+
+if(carry)
+begin
+ Difference = sum + carry;
+ Signbit = 0;
+end
+
+else
+begin
+ Difference = ~sum;
+ Signbit = 1;
+end
+  
+end
+endmodule
