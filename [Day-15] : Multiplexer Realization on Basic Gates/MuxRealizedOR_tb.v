2b14018f49233528963441d605143f4ff16f5a83
Jagadeesh-Siva <142863935+Jagadeesh-Siva@users.noreply.github.com>
2024-07-17 22:38:44 +0530

    Create MuxRealizedOR_tb.v

---
+++ [Day-15] : Multiplexer Realization on Basic Gates/MuxRealizedOR_tb.v
@@ -0,0 +1,11 @@
+module MuxRealizedOR_tb;
+reg A,B; wire F; 
+MuxRealizedOR x1(F,A,B);
+initial begin
+{A,B} = 2'b00; #3
+{A,B} = 2'b01; #3
+{A,B} = 2'b10; #3
+{A,B} = 2'b11; #3
+$stop;
+end
+endmodule
